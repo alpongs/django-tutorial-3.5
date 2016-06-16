098fcd5af812926889a2c81e33adf3f701c2d063
IL YOON <tingcobell@gmail.com>
2016-06-16 17:37:45 +0900

    중간에 migrate 하다가 잘못되어 다시 시작.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-import polls
 from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('polls.urls')),        # / 경로를 표시
-    url(r'^polls/', include('polls.urls')), # /polls/ 경로로 표시
 ]
